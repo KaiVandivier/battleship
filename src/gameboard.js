d8e5bf8af7773b36f7a294348d1b3867e11a44a9
Kai Vandivier <kpvandivier@gmail.com>
2019-10-15 19:40:32 +0200

    Implement and test gameboard

---
+++ src/gameboard.js
@@ -0,0 +1,42 @@
+const gameboard = function gameboard(ship) {
+  const size = 5;
+  const ships = [];
+  const misses = [];
+  const hits = [];
+  const placeShips = () => {
+    ships.push(
+      { ship: ship(4), coords: [ [1,0], [2,0], [3,0], [4,0] ] },
+      { ship: ship(3), coords: [ [1,1], [2,1], [3,1]] },
+      { ship: ship(3), coords: [ [0,2], [1,2], [2,2] ]},
+      { ship: ship(2), coords: [ [3,2], [4,2] ] },
+      { ship: ship(2), coords: [ [0,0], [0,1] ] },
+    );
+  };
+  const receiveAttack = (x, y) => {
+    let hitIndex;
+    const targetHit = ships.find(({ coords }) => {
+      hitIndex = coords.findIndex((coord) => {
+        return x === coord[0] && y === coord[1];
+      })
+      return hitIndex !== -1;
+    });
+    if (targetHit) {
+      targetHit.ship.hit(hitIndex);
+      hits.push([x,y]);
+    } 
+    else 
+      misses.push([x, y]);
+  };
+  const allSunk = () => ships.every(({ ship }) => ship.isSunk());
+  return {
+    size,
+    ships,
+    misses,
+    hits,
+    placeShips,
+    receiveAttack,
+    allSunk,
+  }
+}
+
+export default gameboard;
---
+++ src/gameboard.test.js
@@ -0,0 +1,35 @@
+import gameboard from "./gameboard";
+import ship from "./ship";
+
+test("1: it places ships", () => {
+  const newGameboard = gameboard(ship);
+  expect(newGameboard.ships.length).toBe(0);
+  newGameboard.placeShips();
+  expect(newGameboard.ships.length).toBe(5);
+})
+
+test.todo("#: it places ships that don't overlap");
+
+test("2: it receives attacks from coords and adds misses", () => {
+  const newGameboard = gameboard(ship);
+  newGameboard.receiveAttack(3, 4, null);
+  expect(newGameboard.misses).toContainEqual([3,4]);
+});
+
+test("3: it checks if all ships are sunk (false)", () => {
+  const newGameboard = gameboard(ship);
+  newGameboard.placeShips();
+  expect(newGameboard.allSunk()).toBe(false);
+});
+
+test("4: it checks if all ships are sunk (true)", () => {
+  const newGameboard = gameboard(ship);
+  newGameboard.placeShips();
+  // hit every cell
+  for (let y = 0; y < newGameboard.size; y++) {
+    for (let x = 0; x < newGameboard.size; x++) {
+      newGameboard.receiveAttack(x, y);
+    }
+  }
+  expect(newGameboard.allSunk()).toBe(true);
+});
