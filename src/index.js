9bae97e0e32b8029ce20758bb26301901f58baf1
Kai Vandivier <kpvandivier@gmail.com>
2019-10-17 18:07:27 +0200

    Clean up a bit

--- src/index.js
+++ src/index.js
@@ -3,40 +3,46 @@
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-/* ---- A mockup for UI testing ---- */
+import player from "./player";
 import gameboard from "./gameboard";
 import ship from "./ship";
-import player from "./player";
 import UI from "./ui";
 
-const gb1 = gameboard(ship, 1);
-const gb2 = gameboard(ship, 2);
-gb1.placeShips();
-gb2.placeShips();
-UI.render(gb1, gb2);
+import Game from "./game";
 
-const currentPlayer = player("human");
-const currentTarget = gb2;
+const newGame = new Game(player, gameboard, ship, UI);
+newGame.gameLoop();
 
-const testMove = async function() {
-  const attack = await currentPlayer.getMove(
-    currentTarget, 
-    UI.activateButtons
-  );
-  console.log("attack: ", attack);
-  currentTarget.receiveAttack(attack); //`attack`
-  console.log("hits: ", currentTarget.hits);
-  console.log("misses: ", currentTarget.misses);
+
+/* ---- A mockup for UI testing ---- 
+function testUI() {
+  const gb1 = gameboard(ship, 1);
+  const gb2 = gameboard(ship, 2);
+  gb1.placeShips();
+  gb2.placeShips();
   UI.render(gb1, gb2);
+  
+  const currentPlayer = player("human");
+  const currentTarget = gb2;
+  
+  const testMove = async function() {
+    const attack = await currentPlayer.getMove(
+      currentTarget, 
+      UI.activateButtons
+    );
+    console.log("attack: ", attack);
+    currentTarget.receiveAttack(attack); //`attack`
+    console.log("hits: ", currentTarget.hits);
+    console.log("misses: ", currentTarget.misses);
+    UI.render(gb1, gb2);
+  }
+  
+  testMove()
+    .then(() => testMove())
+    .then(() => testMove())
+    .then(() => testMove());
 }
-
-testMove()
-  .then(() => testMove())
-  .then(() => testMove())
-  .then(() => testMove());
 /* --------------------------------- */
-
-
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/ui.js
+++ src/ui.js
@@ -1,38 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Gameboard from "./Gameboard";
+import Gameboard from "./GameboardComponent";
 
 const UI = (function ui() {
   const render = function render(gameboard1, gameboard2) {
     [gameboard1, gameboard2].forEach(({ id, size, hits, misses, ships }) => {
       const container = document.getElementById(`gb${id}`);
-      // todo: determine which buttons are active
       ReactDOM.render(<Gameboard 
         size={size}
         hits={hits}
         misses={misses}
         ships={ships}
         showShips={id === 1}
-        buttonsActive={false}
-      />, container); // todo: change container
-    }) // will this just repeat over and over, without replacing itself? nei; "diff"
+      />, container);
+    })
   };
+
   const activateButtons = function activateButtons(gameboard, clickHandler) {
     const { id, size, hits, misses, ships } = gameboard;
     const container = document.getElementById(`gb${id}`);
-    console.log("got to `activate buttons`")
-    console.log("click handler:", clickHandler);
     ReactDOM.render(<Gameboard
       size={size}
       hits={hits}
       misses={misses}
       ships={ships}
       showShips={id === 1}
-      buttonsActive={true}
       clickHandler={clickHandler}    
-    />, container)
-
+    />, container);
   };
+
   return {
     render,
     activateButtons
