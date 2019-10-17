f9b60ce21b1031ac59ec9f72861227773beb9437
Kai Vandivier <kpvandivier@gmail.com>
2019-10-17 16:24:29 +0200

    ui prototype

---
+++ src/ui.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Gameboard from "./Gameboard";
+
+const UI = (function ui() {
+  const render = function render(gameboard1, gameboard2) {
+    [gameboard1, gameboard2].forEach(({ id, size, hits, misses, ships }) => {
+      const container = document.getElementById(`gb${id}`);
+      // todo: determine which buttons are active
+      ReactDOM.render(<Gameboard 
+        size={size}
+        hits={hits}
+        misses={misses}
+        ships={ships}
+        showShips={id === 1}
+        buttonsActive={false}
+      />, container); // todo: change container
+    }) // will this just repeat over and over, without replacing itself? nei; "diff"
+  };
+  const activateButtons = function activateButtons(gameboard, clickHandler) {
+    const { id, size, hits, misses, ships } = gameboard;
+    const container = document.getElementById(`gb${id}`);
+    ReactDOM.render(<Gameboard
+      buttonsActive={true}
+      clickHandler={clickHandler}    
+    />, container)
+
+  };
+  return {
+    render,
+    activateButtons
+  }
+})();
+
+export default UI;
--- src/ui.test.js
+++ src/ui.test.js
@@ -1,1 +1,13 @@
-test.todo("");
+import UI from "./ui";
+
+// it's difficult to test DOM and React components
+
+xit("it calls without error", () => {
+  const dummyObj = {
+    hits: [],
+    misses: [],
+    size: 3
+  }
+  expect(UI.render(dummyObj, dummyObj))
+  // this may not work with react elements; see the test in App.test.js
+});
